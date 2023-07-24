8c21b985c16a49cdc39c27d8eaecfdfc6289985a
DavidAdjei <97539347+DavidAdjei@users.noreply.github.com>
2023-07-24 10:52:29 +0000

    update

--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// store data
 export const storeData = async (key, value) => {
   try {
     await AsyncStorage.setItem(key, value);
@@ -8,7 +9,7 @@ export const storeData = async (key, value) => {
   }
 };
 
-
+// get stored data
 export const getData = async (key) => {
     try {
       const value = await AsyncStorage.getItem(key);
